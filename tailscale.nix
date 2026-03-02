0373d7a2435c196129066cbc64d3b58c8d5644ba
gusjengis <anthony.j.green@outlook.com>
2026-03-01 20:49:01 -0800

    removed conditional compilation

--- tailscale.nix
+++ tailscale.nix
@@ -6,69 +6,63 @@
 }:
 {
 
-  options = {
-    tailscale.enable = lib.mkEnableOption "enables tailscale";
+  services.tailscale = {
+    enable = true;
+    openFirewall = true;
   };
 
-  config = lib.mkIf config.tailscale.enable {
-    services.tailscale = {
-      enable = true;
-      openFirewall = true;
-    };
+  environment.systemPackages = with pkgs; [
+    xwayland-satellite
+    waypipe
+    wofi
+  ];
 
-    environment.systemPackages = with pkgs; [
-      xwayland-satellite
-      waypipe
-      wofi
+  # graphics so remote guis work
+  hardware.graphics.enable = true;
+  hardware.graphics.enable32Bit = pkgs.stdenv.hostPlatform.isx86;
+
+  # create a oneshot job to authenticate to Tailscale
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+
+    # make sure tailscale is running before trying to connect to tailscale
+    after = [
+      "network-pre.target"
+      "tailscale.service"
     ];
+    wants = [
+      "network-pre.target"
+      "tailscale.service"
+    ];
+    wantedBy = [ "multi-user.target" ];
 
-    # graphics so remote guis work
-    hardware.graphics.enable = true;
-    hardware.graphics.enable32Bit = pkgs.stdenv.hostPlatform.isx86;
+    # set this service as a oneshot job
+    serviceConfig.Type = "oneshot";
 
-    # create a oneshot job to authenticate to Tailscale
-    systemd.services.tailscale-autoconnect = {
-      description = "Automatic connection to Tailscale";
+    # have the job run this shell script
+    script = with pkgs; ''
+      # wait for tailscaled to settle
+      sleep 2
 
-      # make sure tailscale is running before trying to connect to tailscale
-      after = [
-        "network-pre.target"
-        "tailscale.service"
-      ];
-      wants = [
-        "network-pre.target"
-        "tailscale.service"
-      ];
-      wantedBy = [ "multi-user.target" ];
+      # check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+      fi
 
-      # set this service as a oneshot job
-      serviceConfig.Type = "oneshot";
+      # otherwise authenticate with tailscale
+      if [ -f /home/dragonflylane/.config/secrets/api_keys/env_vars ]; then
+        source /home/dragonflylane/.config/secrets/api_keys/env_vars
+      fi
 
-      # have the job run this shell script
-      script = with pkgs; ''
-        # wait for tailscaled to settle
-        sleep 2
-
-        # check if we are already authenticated to tailscale
-        status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-        if [ $status = "Running" ]; then # if so, then do nothing
-          exit 0
-        fi
-
-        # otherwise authenticate with tailscale
-        if [ -f /home/dragonflylane/.config/secrets/api_keys/env_vars ]; then
-          source /home/dragonflylane/.config/secrets/api_keys/env_vars
-        fi
-
-        if [ -z "''${TAILSCALE_AUTH_KEY:-}" ]; then
-          echo "TAILSCALE_AUTH_KEY missing; skipping tailscale up"
-          exit 0
-        fi
+      if [ -z "''${TAILSCALE_AUTH_KEY:-}" ]; then
+        echo "TAILSCALE_AUTH_KEY missing; skipping tailscale up"
+        exit 0
+      fi
 
 
-        ${tailscale}/bin/tailscale up -authkey "$TAILSCALE_AUTH_KEY"
-        # --ssh --accept-dns=true
-      '';
-    };
+      ${tailscale}/bin/tailscale up -authkey "$TAILSCALE_AUTH_KEY"
+      # --ssh --accept-dns=true
+    '';
   };
 }
